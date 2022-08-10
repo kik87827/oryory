60b15d93b86a6d335c3cf84d10deb07999727e66
kiminkyung <kik87827@gmail.com>
2022-08-11 03:25:19 +0900

    수정 추가 작업중 5

--- dist/js/front.js
+++ dist/js/front.js
@@ -603,8 +603,4 @@ function noticePopup() {
     date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
     document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
   };
-  var getCookie = function(name) {
-    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return value ? value[2] : null;
-  };
 }
